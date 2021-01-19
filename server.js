918f98f86fbabe44378d265e90482228ba5c06df
Spencer Vaughan <spencer.vaughan@gmail.com>
2021-01-18 19:55:27 -0500

    added function to start the server

--- server.js
+++ server.js
@@ -8,3 +8,14 @@ const PORT = 8080;
 // Set up the Express ap to handle data parsing
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
+
+
+
+
+// ROUTES
+
+// Start the server
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
