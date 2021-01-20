19cc9d1f4c44ee4b0b48bc394facee537fe31758
Spencer Vaughan <spencer.vaughan@gmail.com>
2021-01-19 19:45:25 -0500

    installed the npm package uuid and added it as a dependency

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
+const { v4: uuidv4 } = require('uuid');
 
 //Set up Express app
 const app = express();
